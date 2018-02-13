de020520f30f9e92686d29666b2372a4b71ef433
Yash Jaitawat <yashjaitawat@users.noreply.github.com>
2018-02-14 00:18:36 +0530

    Initial commit

---
+++ app/src/main/java/yash/example/com/loginoi/app/AppConfig.java
@@ -0,0 +1,13 @@
+package yash.example.com.loginoi.app;
+
+/**
+ * Created by Yash on 2/13/2018.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://127.0.0.1//android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://127.0.0.1//android_login_api/register.php";
+
+}
